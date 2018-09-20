920be59e187837e3b46ddb71b50e3d440bfab9a8
Amalia <amaliatsa@gmail.com>
2018-09-21 02:06:19 +0300

    Push test

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Report");
+//        System.out.println("Report");
         //Initialize data
         List<Record> records = Utils.generateRecords();
         Utils.calculateIncomingUDSPerDay(records);
